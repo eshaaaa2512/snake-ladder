c976c6a8f1d3419aa75afef2307341139137eb67
Esha <eshadeswal@gmail.com>
2024-01-19 16:25:58 +0530

    starting from position 0

---
+++ UC1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake_ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerposition = 0;
+            Console.WriteLine("Hello,Welcome to snake and ladder game");
+
+            while(playerposition < 100)
+            {
+                Console.WriteLine("Current position of the player:" + (playerposition));
+                Console.WriteLine("Roll the dice");
+                Console.ReadLine();
+
+                
+
+
+
+
+                
+
+
+            }
+
+        }
+    }
+}
