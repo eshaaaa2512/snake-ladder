08571a7967de2a2f63deccacd21a11d60dd0df69
Esha <eshadeswal@gmail.com>
2024-01-20 09:17:01 +0530

    snake , ladder , no play

--- UC1.cs
+++ UC1.cs
@@ -8,27 +8,76 @@ namespace snake_ladder
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
+
+
         {
             int playerposition = 0;
             Console.WriteLine("Hello,Welcome to snake and ladder game");
 
-            while(playerposition < 100)
+            while (playerposition < 100)
             {
                 Console.WriteLine("Current position of the player:" + (playerposition));
                 Console.WriteLine("Roll the dice");
                 Console.ReadLine();
 
+                int playerRandomNum;
+                Random random = new Random();
+                 
+                {
+                    playerRandomNum = random.Next(1, 7);
+                    Console.WriteLine("Outcome : " + (playerRandomNum));
+                    Console.ReadLine();
+                }
                 
+                int option = random.Next(0, 3);
+
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("No Play. Stay in the same position.");
+                        break;
+
+                    case 1:
+                        Console.WriteLine($"Ladder! Move ahead by " + playerRandomNum + "positions.");
+                        playerposition += playerRandomNum;
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Snake! Move behind by " + playerRandomNum + " positions.");
+                        playerposition -= playerRandomNum;
+
+
+                        playerposition = Math.Max(playerposition, 0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option.");
+                        break;
+                }
 
 
 
 
-                
-
-
+                Console.ReadLine();
             }
-
         }
     }
 }
+
+            
+
+        
+
+
+
+
+
+
+
+
+            
+
+        
+    
+
